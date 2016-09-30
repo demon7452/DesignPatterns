6c7cff2ce9b45919d8516e5532da191baeaa94f4
demon7452 <demonli7452@gmail.com>
2016-09-30 17:30:02 +0800

    package info

---
+++ src/main/java/com/patterns01/strategy/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Function： Strategy Pattern
+ * 策略模式
+ *
+ *   ver     date      	author
+ * ──────────────────────────────────
+ *   		2016年9月30日     lixiong
+ *
+ * Copyright (c) 2016, WKZF All Rights Reserved.
+*/
+
+package com.patterns01.strategy;
